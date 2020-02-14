516f628fbde876652966fdf6c90bf00d01b310da
ER123 <lishuaijie123@163.com>
2020-02-14 22:19:23 +0800

    delete files

--- 05 - Longest Palindromic Substring/solution.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-
-class Solution {
-public:
-	std::string longestPalindrome(std::string s);
-};
--- 06 07 ZigZag Conversion  - Reverse Integer/solution.hpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-class Solution{
-public:
-	std::string convert(std::string s, int numRows);
-	int resver_intger(int num);
-};
